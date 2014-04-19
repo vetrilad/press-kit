6e08dc07ff76e3d29c7cbc3ecbfa8d6498772a25
bumbu <bmbalex@gmail.com>
2014-04-19 17:21:22 +0300

    add visualization

---
+++ visualization/js/application.js
@@ -0,0 +1,22 @@
+$(function () {
+  $('#words-frequency-visualization').words_mentions({
+    images: {
+      "V. Filat": "images/filat.png"
+    , "V. Voronin": "images/voronin.png"
+    , "A. Tănase": "images/tanase.png"
+    , "I. Dodon": "images/dodon.png"
+    , "M. Ghimpu": "images/ghimpu.png"
+    , "S. Urechean": "images/urechean.png"
+    , "M. Lupu": "images/lupu.png"
+    , "I. Leancă": "images/leanca.png"
+    , "V. Plahotniuc": "images/plahotniuc.png"
+    , "D. Chirtoacă": "images/chirtoaca.png"
+    , "M. Godea": "images/godea.png"
+    , "D. Diacov": "images/diacov.png"
+    , "V. Streleţ": "images/strelet.png"
+    , "S. Sârbu": "images/sarbu.png"
+    , "I. Hadârcă": "images/hadarca.png"
+    },
+    data_source: '../dataset.json'
+  })
+});
