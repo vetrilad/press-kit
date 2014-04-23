b4088d603d4aa6df4deea7cc0dad91ec4f69b8c2
Ivan Zarea <zarea.ion@gmail.com>
2014-04-23 23:07:19 +0300

    Squashing only the files that are needed

--- squasher.rb
+++ squasher.rb
@@ -1,14 +1,53 @@
 require 'json'
+require 'i18n'
 
 PARSED_DIR   = "data/parsed/"
 SQUASHED_DIR = "data/squashed/"
+
+Person = Struct.new(:key, :name, :terms)
+@people = [
+  Person.new(:filat,   "V. Filat",       ["Filat"]),
+  Person.new(:voronin, "V. Voronin",     ["Voronin"]),
+  Person.new(:tanase,  "A. Tănase",      ["Tanase"]),
+  Person.new(:dodon,   "I. Dodon",       ["Dodon"]),
+  Person.new(:ghimpu,  "M. Ghimpu",      ["Ghimpu"]),
+  Person.new(:urechean,  "S. Urechean",  ["Urechean"]),
+  Person.new(:lupu,  "M. Lupu",          ["Lupu"]),
+  Person.new(:leanca,  "I. Leancă",      ["Leanca"]),
+  Person.new(:plaho,  "V. Plahotniuc",   ["Plahotniuc"]),
+  Person.new(:chirt,  "D. Chirtoacă",    ["Chirtoaca"]),
+  Person.new(:godea,  "M. Godea",        ["Godea"]),
+  Person.new(:diacov,  "D. Diacov",      ["Diacov"]),
+  Person.new(:strelet,  "V. Streleţ",    ["Strelet"]),
+  Person.new(:sirbu,  "S. Sârbu",        ["Sirbu", "Sarbu"]),
+  Person.new(:hadarca,  "I. Hadârcă",    ["Hadarca", "Hadirca"])
+]
 
 data = []
 
 File.write(SQUASHED_DIR + "all.json", "")
 
+def has_mention?(text)
+  @people.each do |person|
+    person.terms.each do |term|
+      return true unless text.scan(term).empty?
+    end
+  end
+  false
+end
+
 def build_url(id)
   "http://unimedia.info/stiri/-#{id}.html"
+end
+
+most_recent = Dir[PARSED_DIR + "*"].max_by { |f| File.mtime(f) }
+parsed_time = File.mtime(most_recent)
+squashed_time = File.mtime(SQUASHED_DIR + "all.json")
+
+if squashed_time < parsed_time
+  # there's nothing new
+  puts "Nothing new parsed"
+  exit
 end
 
 Dir[PARSED_DIR + "*"].each do |filename|
@@ -17,8 +56,13 @@ Dir[PARSED_DIR + "*"].each do |filename|
   id = filename.gsub(PARSED_DIR, "")
   article["id"]  = id
   article["url"] = build_url(id)
+  next unless article["content"]
 
-  data << article if article["content"]
+  article["content"] = I18n.transliterate(article["content"])
+
+  if has_mention?(article["content"])
+    data << article
+  end
 end
 
 sorted = data.sort_by {|elem| elem["id"].to_i }
