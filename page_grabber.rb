1130634dd112b9ec6a70523d77855a2e02b9b2c3
Ivan Zarea <zarea.ion@gmail.com>
2014-04-27 18:52:41 +0300

    Using a different regex to find the latest article

--- page_grabber.rb
+++ page_grabber.rb
@@ -1,8 +1,8 @@
 require 'rest_client'
 require 'nokogiri'
+require 'pry'
 
 PAGES_DIR      = "data/pages/"
-PER_SECOND     = 15
 
 FileUtils.mkdir_p "data/pages"
 
@@ -12,7 +12,8 @@ def most_recent_id
   url = "http://unimedia.info/rss/news.xml"
 
   doc = Nokogiri::XML(RestClient.get(url))
-  @most_recent_id = doc.css("link")[3].text[-10..-5].to_i
+  binding.pry
+  @most_recent_id = doc.css("link")[3].text.scan(/-([\d]+)\.html.+/).first.first.to_i
 end
 
 def latest_stored_id
@@ -31,7 +32,7 @@ def save(page, id)
 end
 
 def time_left_string(id)
-  time = (most_recent_id - id) / PER_SECOND
+  time = (most_recent_id - id) / 3 #magical number
   minutes, seconds = time.divmod(60)
   hours, minutes   = minutes.divmod(60)
   "#{hours}h #{minutes}m #{seconds}s"
@@ -43,10 +44,11 @@ def fetch_single(id)
   puts "#{id} saved. #{bytes/1024} KB"
 end
 
-(latest_stored_id..most_recent_id).step(PER_SECOND) do |id|
-  PER_SECOND.times do |increment|
-    fetch_single(id + increment)
-  end
+p "Latest stored id is #{latest_stored_id}"
+p "Most recent id is #{most_recent_id}"
+
+(latest_stored_id..most_recent_id).step(1) do |id|
+  fetch_single(id)
   puts (id / most_recent_id.to_f * 100).round(2).to_s + "% done"
   puts time_left_string(id)
   puts
