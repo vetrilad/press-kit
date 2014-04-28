71d786b27f2bf91998c8c4897c49a00f01054291
Ivan Danci <bah4uk@gmail.com>
2014-04-28 06:18:12 +0300

    Fakin BadGateway

--- scripts/fetch.rb
+++ scripts/fetch.rb
@@ -3,7 +3,13 @@ require_relative "../main"
 UnimediaFetcher.new.run
 
 begin
+  tries ||= 200
   TimpulFetcher.new.run
-rescue => e
-  binding.pry
+rescue RestClient::BadGateway => e
+  sleep 2
+  puts "ne zaebisi posoni(("
+  puts "no zaebisi ne daleko!"
+  retry unless (tries -= 1).zero?
+else
+  puts "zaebisi posoni!!"
 end
